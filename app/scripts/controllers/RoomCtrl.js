84486ed27ac63022bd2ec1652eaa2426cf354a5a
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-10-21 00:01:56 -0500

    user-story-4

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,8 +1,9 @@
  (function() {
  
-   function RoomCtrl($scope, $uibModal, Room) {
+   function RoomCtrl($scope, $uibModal, Room, Message) {
   
      $scope.allRooms = Room.all;
+     $scope.activeRoom = null;
      $scope.open = function() {
  
          var modalInstance = $uibModal.open({
@@ -15,9 +16,22 @@
              Room.createRoom(newName);
          })
       }
+      $scope.setRoom = function(room) {
+      $scope.activeRoom = room;
+      $scope.roomName = room.name;
+      $scope.messages = Room.messageFinder(room.$id);
+      }
+      $scope.sendMessage = function() {
+        Message.send({
+            roomId: $scope.activeRoom.$id,
+            sentAt: firebase.database.ServerValue.TIMESTAMP,
+            content: $scope.newMessage
+        });
+        $scope.newMessage = '';
+      }
     }
   
     angular
       .module('blocChat')
-      .controller('RoomCtrl', ['$scope', '$uibModal', 'Room', RoomCtrl]);
+      .controller('RoomCtrl', ['$scope', '$uibModal', 'Room', 'Message', RoomCtrl]);
   })();

86d2cb94e3a4f28cac2e4ba84d3f423660e4d014
alpeterson24 <alpeterson2424@yahoo.com>
2017-03-05 18:00:22 -0600

    controller changes and screen shots

--- app/scripts/Services/Room.js
+++ app/scripts/Services/Room.js
@@ -4,22 +4,22 @@
       var mesRef = firebase.database().ref().child("messages").orderByChild('roomId');
       var rooms = $firebaseArray(ref);
       var messages = $firebaseArray(mesRef);
-  
+
       var addChatRoom = function(newRoom) {
       rooms.$add({name: newRoom});
       };
-       
+
       var findMessages = function(roomId) {
       return $firebaseArray(mesRef.equalTo(roomId));
       };
- 
+
       return {
        all: rooms,
        createRoom: addChatRoom,
        messageFinder: findMessages
       };
-    }
-  
+   }
+
    angular
      .module('blocChat')
      .factory('Room', ['$firebaseArray', Room]);
--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,17 +1,16 @@
  (function() {
- 
    function RoomCtrl($scope, $uibModal, Room, Message, $cookies) {
-  
-     $scope.allRooms = Room.all;
-     $scope.activeRoom = null;
-     $scope.open = function() {
- 
+
+      $scope.allRooms = Room.all;
+      $scope.activeRoom = null;
+      $scope.open = function() {
+
          var modalInstance = $uibModal.open({
              animation: true,
              templateUrl: '/templates/newRoom.html',
              controller: 'ModalCtrl'
          });
- 
+
          modalInstance.result.then(function (newName) {
              Room.createRoom(newName);
          })
@@ -30,9 +29,9 @@
         });
         $scope.newMessage = '';
       }
-    }
+   }
   
-    angular
+   angular
       .module('blocChat')
       .controller('RoomCtrl', ['$scope', '$uibModal', 'Room', 'Message', '$cookies', RoomCtrl]);
-  })();
+ })();
